fa9e062c6115056176949c373b535175228bc9aa
christ <christophe.arnal@me.com>
2026-03-03 19:11:21 +0100

    Ajout des préférences utilisateur

--- src/api/index.js
+++ src/api/index.js
@@ -48,8 +48,8 @@ export const scrutateurAPI = {
     api.post(`/bureaux/${bureauId}/participations`, { heure, votants }),
   upsertResultat: (bureauId, data) =>
     api.post(`/bureaux/${bureauId}/resultats`, data),
-  getUser:    (id) => api.get(`/users/${id}`),
-  updateUser:    (id) => api.post(`/users/${id}`, data),
+  getUser:    (id) => api.get(`/user/${id}`),
+  updateUser:    (id) => api.post(`/user/${id}`, data),
 }
 
 // ===== ADMIN =====
