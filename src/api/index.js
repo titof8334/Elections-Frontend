fad9816a19a3f1bf8f63258298bbb3b09715c892
christ <christophe.arnal@me.com>
2026-03-04 02:37:52 +0100

    Ajout de nouveaux rôles

--- src/api/index.js
+++ src/api/index.js
@@ -64,6 +64,7 @@ export const adminAPI = {
   // Users
   getUsers:    () => api.get('/users'),
   createUser:  (data) => api.post('/users', data),
+  updateUser:  (id,data) => api.put('/users/${id}', data),
   deleteUser:  (id) => api.delete(`/users/${id}`),
 
   // Candidats
